7b265ef43cef777590b3d6bf66bd5db5d312e391
i99dev <i99dev@i99.dev>
2026-05-06 06:30:40 +0400

    Add initial files for ADB Helper mini-app
    - Created main JavaScript chunk for the application.
    - Added CSS styles for the app layout and components.
    - Included an SVG icon for the app branding.
    - Generated the main HTML file for the app interface.
    - Created an index file for Next.js routing and rendering.

---
+++ examples/adb-helper-style/out/_next/static/chunks/app/layout-7b04e8703d803713.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{3388:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,9821,23))},9821:()=>{}},e=>{e.O(0,[950,623,338,358],()=>e(e.s=3388)),_N_E=e.O()}]);
